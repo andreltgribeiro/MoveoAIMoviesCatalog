827fca609641b73b790c761b76a30f8316c77809
andreltgribeiro <andreltgribeiro@gmail.com>
2026-01-07 01:03:29 -0300

    Adjusting movie tile responsiveness

--- src/components/MovieTile/index.jsx
+++ src/components/MovieTile/index.jsx
@@ -20,12 +20,12 @@ function MovieTile({ movie = {} }) {
   return (
     <div
       key={movie.id}
-      className="h-84 cursor-pointer flex border border-gray-500 rounded-2xl overflow-hidden w-135 hover:shadow-2xl/30 hover:-translate-y-0.5 transition duration-300 shadow-gray-100 "
+      className="w-[90%] lg:w-[32%] aspect-video cursor-pointer flex border border-gray-500 rounded-2xl overflow-hidden hover:shadow-2xl/30 hover:-translate-y-0.5 transition duration-300 shadow-gray-100 "
     >
       <img
         src={movie.poster_path}
         loading="lazy"
-        className={`min-w-55 ${mutableOpacityClasses}`}
+        className={`min-w-[40%] ${mutableOpacityClasses}`}
         onLoad={() => setPosterLoaded(true)}
       />
       <div className={`relative w-full`}>
@@ -36,14 +36,19 @@ function MovieTile({ movie = {} }) {
         />
         <div className="p-4 h-full text-zinc-50 backdrop-blur-lg bg-zinc-800/40 flex flex-col items-start justify-end gap-0.5">
           <small>{getYearFromReleaseDate(movie.release_date)}</small>
-          <p className="text-2xl font-bold">{movie.original_title}</p>
-          <p className="max-h-50 line-clamp-3" title={movie.overview}>
+          <p className="text-xl md:text-2xl font-bold">
+            {movie.original_title}
+          </p>
+          <p
+            className="text-xs md:text-sm max-h-50 line-clamp-3"
+            title={movie.overview}
+          >
             {movie.overview}
           </p>
           {movie.vote_count > 0 ? (
             <div className="flex items-center gap-0.5">
               <StarIcon className="size-4 text-yellow-300" />{" "}
-              {movie.vote_average}
+              <p className="text-xs md:text-sm">{movie.vote_average}</p>
               <small>({movie.vote_count})</small>
             </div>
           ) : (
