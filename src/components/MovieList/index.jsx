49d2823b13ca593505d22714b9d9a74ac4daea69
andreltgribeiro <andreltgribeiro@gmail.com>
2026-01-06 22:47:23 -0300

    Start styling movies

--- src/components/MovieList/index.jsx
+++ src/components/MovieList/index.jsx
@@ -2,15 +2,15 @@ import MovieTile from "../MovieTile";
 import NoData from "../NoData";
 
 function MovieList({ movies = [] }) {
-  if (!movies?.pages?.length) return <NoData />;
+  if (!movies?.length) return <NoData />;
 
-  return movies.pages.map((page, pageIndex) => (
-    <ul key={pageIndex}>
-      {page.data.map((movie) => (
+  return (
+    <div className="flex flex-wrap gap-4 justify-center">
+      {movies.map((movie) => (
         <MovieTile key={movie.id} movie={movie} />
       ))}
-    </ul>
-  ));
+    </div>
+  );
 }
 
 export default MovieList;
--- src/components/MovieTile/index.jsx
+++ src/components/MovieTile/index.jsx
@@ -1,8 +1,23 @@
+import { StarIcon } from "@heroicons/react/24/solid";
+
 function MovieTile({ movie = {} }) {
   return (
-    <li key={movie.id}>
-      <strong>{movie.original_title}</strong> ({movie.release_date})
-    </li>
+    <div
+      key={movie.id}
+      className="relative flex border border-gray-500 rounded-2xl overflow-hidden w-135"
+    >
+      <img src={movie.poster_path} loading="lazy" className="w-55" />
+      <div className="p-4 text-zinc-50">
+        <strong>{movie.original_title}</strong>
+        <br />
+        <small>({movie.release_date})</small>
+        <br />
+        <div className="flex items-center gap-0.5">
+          <StarIcon className="size-3" /> {movie.vote_average}
+          <small>({movie.vote_count})</small>
+        </div>
+      </div>
+    </div>
   );
 }
 
